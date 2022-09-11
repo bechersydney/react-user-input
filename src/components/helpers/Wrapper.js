fcaeb3eab0b35783fcc5b6fbe578031fd91e3b41
sydney <becher.sydney@gmail.com>
2022-09-11 15:02:04 +0800

    add new dev

--- src/components/helpers/Wrapper.js
+++ src/components/helpers/Wrapper.js
@@ -2,6 +2,7 @@ const Wrapper = (props) => {
     // this is the component wrapper
     // new comment
     // I am adding here 
+    // add new dev
     return props.children;
 };
 export default Wrapper;
